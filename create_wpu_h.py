0f22e8a4da657849193b341136b5ddee863e3af2
Tom Kooij <tomkooij@tomkooij.nl>
2024-03-14 10:21:04 +0100

    Tweak statuslabels

--- create_wpu_h.py
+++ create_wpu_h.py
@@ -27,55 +27,14 @@ fw_versions = {} # fw_versions['datalabel'] = [2, 4, ... 34, 37, 41]
 # Translated labels, to export into wpu.h
 StatusLabelNames = []
 
+#
+# Read MDB
+#
 
 def get_version(s):
     x = re.findall('\d+', s)
     assert len(x) == 1
     return int(x[0])
-
-
-def itho_WPU_status_line(fw_ver, l):
-    # const uint8_t itho_WPUstatus42[]{0, 1, 255};
-    indices_str = ', '.join(str(x) for x in l)
-    return f'const uint8_t itho_WPUstatus{fw_ver}[]{{{indices_str}, 255}};'
-
-
-# Generate the index tables 'itho_WPUstatus' from the parameter tables
-def print_itho_WPU_status_lines():
-    for fw_ver in fw_versions['datalabels']:
-        fw_ver = int(fw_ver)
-        #print(fw_ver)
-        label_list = []
-        for label in WPUStatusLabels[fw_ver]:
-            idx, name, _,_,_,_ = label
-            #print(idx, name)
-            if name not in StatusLabelNames:
-                StatusLabelNames.append(name)
-            list_idx = StatusLabelNames.index(name)
-            label_list.append(list_idx)
-        print(itho_WPU_status_line(fw_ver, label_list))
-
-
-def create_lookup_table_statuslabels():
-    Labels = {}
-
-    # create lookup tabel for Statuslabels, based on most recent firmware
-    for fw in fw_versions['datalabels']:
-        for label in WPUStatusLabels[fw]:
-            idx, name, nl_tekst, nl_unit, gb_tekst, gb_unit = label
-            #print(name, gb_tekst, gb_unit)
-            if gb_tekst is None:
-                gb_tekst = nl_tekst
-                gb_unit = nl_unit
-            if gb_unit is None:
-                unit = ""
-            else:
-                unit = " ("+gb_unit+")"
-            desc = gb_tekst+unit
-            # create a slug. Hand generated slugs use C instead of degc for temperature units
-            slug = str(slugify.slugify(desc)).replace('degc', 'c')
-            Labels[name] = (desc,slug)
-    return Labels
 
 
 def read_mdb(DRV=DRV, PARFILE=PARFILE, PASSWORD=PASSWORD):
@@ -130,6 +89,53 @@ def read_mdb(DRV=DRV, PARFILE=PARFILE, PASSWORD=PASSWORD):
     fw_versions['settings'] = sorted([int(x) for x in SettingLabels.keys()])
     
 
+#
+# StatusLabels
+# 
+def itho_WPU_status_line(fw_ver, l):
+    # const uint8_t itho_WPUstatus42[]{0, 1, 255};
+    indices_str = ', '.join(str(x) for x in l)
+    return f'const uint8_t itho_WPUstatus{fw_ver}[]{{{indices_str}, 255}};'
+
+
+# Generate the index tables 'itho_WPUstatus' from the parameter tables
+def print_itho_WPU_status_lines():
+    for fw_ver in fw_versions['datalabels']:
+        fw_ver = int(fw_ver)
+        #print(fw_ver)
+        label_list = []
+        for label in WPUStatusLabels[fw_ver]:
+            idx, name, _,_,_,_ = label
+            #print(idx, name)
+            if name not in StatusLabelNames:
+                StatusLabelNames.append(name)
+            list_idx = StatusLabelNames.index(name)
+            label_list.append(list_idx)
+        print(itho_WPU_status_line(fw_ver, label_list))
+
+
+def create_lookup_table_statuslabels():
+    Labels = {}
+
+    # create lookup tabel for Statuslabels, based on most recent firmware
+    for fw in fw_versions['datalabels']:
+        for label in WPUStatusLabels[fw]:
+            idx, name, nl_tekst, nl_unit, gb_tekst, gb_unit = label
+            #print(name, gb_tekst, gb_unit)
+            if gb_tekst is None:
+                gb_tekst = nl_tekst
+                gb_unit = nl_unit
+            if gb_unit is None:
+                unit = ""
+            else:
+                unit = " ("+gb_unit+")"
+            desc = gb_tekst+unit
+            # create a slug. Hand generated slugs use C instead of degc for temperature units
+            slug = str(slugify.slugify(desc)).replace('degc', 'c')
+            Labels[name] = (desc,slug)
+    return Labels
+
+
 def print_ithoWPUStatusLabels():
     # print the StatusLabels, import the handmade translations first
     #  generate new one from tekst_gb if new
@@ -137,7 +143,7 @@ def print_ithoWPUStatusLabels():
     # Lookup table 'Naam' -> ('tekst_gb', 'eenheid_gb')
     Labels = create_lookup_table_statuslabels()
 
-    print("const struct ithoLabels ithoWPUStatusLabels[] { \t//index".expandtabs(120))
+    print("const struct ithoLabels ithoWPUStatusLabels[]{ \t//index".expandtabs(120))
     for idx,label in enumerate(StatusLabelNames):
         try:
             # replace with the "old" handmade translation
@@ -149,10 +155,17 @@ def print_ithoWPUStatusLabels():
         print(f'    {{"{desc}", "{slug}"}},\t//{idx}'.expandtabs(120))
 
 
+#
+# SettingLabels        
+#
+
+
+
 
 if __name__ == '__main__':
     read_mdb()
     print("//DEBUG: ", fw_versions)
     print("// diff StatusLabels against wpu.h. Only 8_9 11_17 and 18_19 are different. All keys should be the same")
     print_itho_WPU_status_lines()
+    print()
     print_ithoWPUStatusLabels()
